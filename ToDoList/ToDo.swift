14390564761de5be0e407916bbe985be98c8e315
nimet <apple@Apples-MacBook-Pro-59.local>
2019-06-24 13:42:40 -0500

    Iteration 0 final

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Apple on 6/24/19.
+//  Copyright © 2019 Nimet. All rights reserved.
+//
+
+import UIKit
+class ToDo {
+    var name = ""
+    var important = false
+}
+
